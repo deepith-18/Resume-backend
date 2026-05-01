5c70e5814ca2064fb75041d54cea883f054db249
Deepith N <deepithdeekshith@gmail.com>
2026-05-01 15:23:17 +0530

    Fix-6

--- server/models/Resume.js
+++ server/models/Resume.js
@@ -1,28 +1,25 @@
 const mongoose = require('mongoose');
 
-const resumeSchema = new mongoose.Schema(
-  {
-    fileName: String,
-    filePath: String,
-    fileSize: Number,
-    fileType: String,
-    rawText: String,
+const resumeSchema = new mongoose.Schema({
+  fileName: String,
+  filePath: String,
+  fileSize: Number,
+  fileType: String,
+  rawText: String,
 
-    parsedData: {
-      name: String,
-      skills: [
-        {
-          name: String,
-          category: String,
-          proficiency: String,
-        },
-      ],
-      experienceLevel: String,
-    },
-
-    analysisStatus: String,
+  parsedData: {
+    name: String,
+    skills: [
+      {
+        name: String,
+        category: String,
+        proficiency: String,
+      },
+    ],
+    experienceLevel: String,
   },
-  { timestamps: true }
-);
 
-module.exports = mongoose.model('Resume', resumeSchema);
+  analysisStatus: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model('Resume', resumeSchema);getResume()
